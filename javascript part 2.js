997c05c90e68f421d91c6da8333dc1d43cd956df
krishna keshariya <keshariyakrishna*@gmail.com>
2025-10-17 18:04:34 +0530

    complete few tasks

---
+++ javascript part 2.js
@@ -0,0 +1,7 @@
+// change the text of the h1 element
+const h1 = document.querySelector("h1").textContent = "Hello krishna";
+
+//change the background color and font color of the div
+const contentBox = document.querySelector(".content-box");
+contentBox.style.backgroundColor = "gray";
+contentBox.style.color = "red";
